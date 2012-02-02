9b92937d771f53bf51fc07cdb6ecdfcdf0cdedca
Onsite-Tech-Services <aaron@onsitetechservices.com>
2012-02-02 12:16:13 -0500

    Updated from Dufour GA to OG GA

--- includes/analytics.inc.php
+++ includes/analytics.inc.php
@@ -1,5 +1,5 @@
 <script> // Change UA-XXXXX-X to be your site's ID
-  window._gaq = [['_setAccount','UA-7477439-17'],['_trackPageview'],['_trackPageLoadTime']];
+  window._gaq = [['_setAccount','UA-7477439-18'],['_trackPageview'],['_trackPageLoadTime']];
   Modernizr.load({
     load: ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js'
   });
